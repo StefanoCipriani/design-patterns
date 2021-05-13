08143d4c5b02205771a55ed2c3c5749767a9b561
StefanoCipriani <stefano.cipriani@dxc.com>
2021-05-13 18:42:04 +0200

    new branch

---
+++ src/main/java/com/design/pattern/sample/Sample.java
@@ -0,0 +1,14 @@
+package com.design.pattern.sample;
+
+public class Sample {
+
+	public void someMethod1() {
+	
+		System.out.println();
+	}
+	
+	public void someMethod3() {
+		
+		System.out.println();
+	}
+}
