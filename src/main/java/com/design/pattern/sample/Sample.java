0711c6276c60292087f096aadd1e926cb9e6b6f6
StefanoCipriani <stefano.cipriani@dxc.com>
2021-05-13 18:53:01 +0200

    new method

--- src/main/java/com/design/pattern/sample/Sample.java
+++ src/main/java/com/design/pattern/sample/Sample.java
@@ -2,26 +2,7 @@ package com.design.pattern.sample;
 
 public class Sample {
 
-	public void someMethod1() {
-	
-		System.out.append("g");
-	}
-	
-	public void someMethod2() {
-		
-		System.out.println();
-	}
-
-	public void someMethod3() {
-		
-		System.out.append("h");
-		System.out.println();
-		System.out.append("1");
-	}
-	
-	
-	public void someMethod4() {
-		
-		System.out.println();
+	public void newMethod() {
+		System.out.println("Nuovo metodo");
 	}
 }
