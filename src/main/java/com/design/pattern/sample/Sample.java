027d01211282900f391a92589f7fea501f2f46d7
StefanoCipriani <stefano.cipriani@dxc.com>
2021-05-13 18:57:24 +0200

    mods

--- src/main/java/com/design/pattern/sample/Sample.java
+++ src/main/java/com/design/pattern/sample/Sample.java
@@ -2,9 +2,23 @@ package com.design.pattern.sample;
 
 public class Sample {
 
-	public void newMethod() {
+	public void newMethoda() {
 		System.out.println("Nuovo metodo");
 	}
 	
+	public void newMethodb() {
+		System.out.println("Nuovo metodo");
+	}
+	
+	public void newMethod() {
+		System.out.println("Nuovo metodo");
+		System.out.println("Nuovo metodo");
+		System.out.println("Nuovo metodo");
+		System.out.println("Nuovo metodo");
+	}
+	
+	public void newMethodc() {
+		System.out.println("Nuovo metodo");
+	}
 	
 }
