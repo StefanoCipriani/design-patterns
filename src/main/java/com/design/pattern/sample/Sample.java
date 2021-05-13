9eb9394940d1d537b813bb26fac339153938c01a
StefanoCipriani <stefano.cipriani@dxc.com>
2021-05-13 18:43:47 +0200

    changes

--- src/main/java/com/design/pattern/sample/Sample.java
+++ src/main/java/com/design/pattern/sample/Sample.java
@@ -4,10 +4,23 @@ public class Sample {
 
 	public void someMethod1() {
 	
-		System.out.println();
+		System.out.append("a");
 	}
 	
+	public void someMethod2() {
+		
+		System.out.println();
+	}
+
 	public void someMethod3() {
+		
+		System.out.append("b");
+		System.out.println();
+		System.out.append("c");
+	}
+	
+	
+	public void someMethod4() {
 		
 		System.out.println();
 	}
